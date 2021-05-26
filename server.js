ce6706189ac6fe50008585f009306cd2495e33e6
Abigail Adams <abigail-adams@gmail.com>
2021-05-26 05:08:17 -0700

    adding routes

--- server.js
+++ server.js
@@ -1,36 +1,36 @@
-const path = require('path');
-const express = require('express');
-const routes = require('./controllers/');
-const sequelize = require('./config/connection');
+const path = require("path");
+const express = require("express");
+const routes = require("./controllers/");
+const sequelize = require("./config/connection");
 // testing
 
-const session = require('express-session');
+const session = require("express-session");
 
-const exphbs = require('express-handlebars');
+const exphbs = require("express-handlebars");
 
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
-const helpers = require('./utils/helpers');
+const helpers = require("./utils/helpers");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const hbs = exphbs.create({ helpers });
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 const sess = {
-    secret: 'Super secret secret',
-    cookie: {
-        maxAge: 60000
-    },
-    resave: false,
-    saveUninitialized: true,
-    rolling: true,
+  secret: "Super secret secret",
+  cookie: {
+    maxAge: 60000,
+  },
+  resave: false,
+  saveUninitialized: true,
+  rolling: true,
 
-    store: new SequelizeStore({
-        db: sequelize
-    })
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
 };
 
 app.use(session(sess));
@@ -38,10 +38,12 @@ app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log(`Now listening at http://localhost:${PORT}`));
+  app.listen(PORT, () =>
+    console.log(`Now listening at http://localhost:${PORT}`)
+  );
 });
